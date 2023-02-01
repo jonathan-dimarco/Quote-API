e2840b6cfced4318d50cc2121249aba6c87a7e49
Jonathan_DiMarco <jonathandimarcoq@gmail.com>
2023-02-01 22:22:44 +0100

    added GET all quotes an POST new quote methods

--- server.js
+++ server.js
@@ -15,7 +15,33 @@ app.get('/api/quotes/random', (req, res) => {
     });
   });
 
-  
+// GET all quotes if an author is not specified in the query
+  app.get('/api/quotes', (req, res) => {
+    if (req.query.person !== undefined) {
+      const quotesByPerson = quotes.filter(quote => quote.person === req.query.person);
+      res.send({
+        quotes: quotesByPerson
+      });
+    } else {
+      res.send({
+        quotes: quotes
+      });
+    }
+  });
+
+// POST a new quote and his author
+  app.post('/api/quotes', (req, res) =>{
+    const newQuote = {
+      quote: req.query.quote,
+      person: req.query.person
+    }
+    if(newQuote.quote && newQuote.person) {
+      quotes.push(newQuote);
+      res.send({ quote: newQuote })
+    } else {
+      res.status(400).send()    
+    }
+  })
 
 
 
